d95c1f3e57b058783a162ed0a5c0e612abafb532
Joseph Lee <shlee.esp@gmail.com>
2020-07-12 21:20:48 -0700

    JS update
    save to local storage

--- Index.js
+++ Index.js
@@ -1,32 +1,46 @@
-const form  = document.querySelector(".js-form"),
-input = form.querySelector("input"),
-greeting= document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
-const USER_LS="currentUser",
-    SHOWING_CN="showing";
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
 
-
-
-function paintGreeting(text){
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
-
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
 }
 
-function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null) {
-        // she is not
-     } else{ paintGreeting(currentUser);
-
-        //she is
-     }
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
 }
 
-function init(){
-loadName();
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
 
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+    // she is not
+  } else {
+    paintGreeting(currentUser);
+
+    //she is
+  }
+}
+
+function init() {
+  loadName();
 }
 
 init();
